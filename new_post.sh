cec1b3b407f8a64a10bb7516120d5a4f03b692c4
Thuyen Trinh <trinhngocthuyen@gmail.com>
2017-07-31 10:25:13 +0700

    Minor change: wrong path to mathjax_config.js, wrong file name

--- new_post.sh
+++ new_post.sh
@@ -4,7 +4,9 @@ POST_DIR="_posts"
 
 POST_NAME="$1"
 DATE=`date +%Y-%m-%d`
-POST_ID="${DATE}"-"${POST_NAME// /-}"
+POST_ID="${POST_NAME//[,;\'\"]/}"
+POST_ID="${POST_ID// - /-}"
+POST_ID="${DATE}-${POST_ID// /-}"
 POST_ID=$(echo "${POST_ID}" | tr '[:upper:]' '[:lower:]')	# Lowercase
 
 FILE="${POST_DIR}/${POST_ID}.md"
