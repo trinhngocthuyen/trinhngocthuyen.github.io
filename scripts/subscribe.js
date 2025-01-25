7f6f7c91cc7215903901cc4663ce66bfccbf934d
trinhngocthuyen <trinhngocthuyen@gmail.com>
2025-01-25 02:33:21 +0000

    deploy new changes at Sat Jan 25 02:33:21 UTC 2025

--- scripts/subscribe.js
+++ scripts/subscribe.js
@@ -1,11 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
   function closeSubscription() {
-    $('.frontpage .subscription').css('display', 'none');
+    $('.frontpage .subscription').css('transform', 'translate(-50%, -50%) scale(0)');
     $('.frontpage .overlay').css('display', 'none');
   }
 
   $('.frontpage .bell').click(function () {
-    $('.frontpage .subscription').css('display', 'block');
+    $('.frontpage .subscription').css('transform', 'translate(-50%, -50%) scale(1)');
     $('.frontpage .overlay').css('display', 'block');
   });
   $('.frontpage .subscription .close').click(closeSubscription);
